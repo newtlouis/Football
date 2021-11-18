d982dac03f440d0beeb2bf976e373b07b4fd323c
Louis <louis.m.newton@hotmail.fr>
2021-11-18 12:54:50 +0100

    CSS nom et centrage de l'image

--- libraries/views/lobby.html.php
+++ libraries/views/lobby.html.php
@@ -21,6 +21,21 @@
           <h2>{{player.player.name}}</h2>
           <i @click="removeItem(id)" class="fas fa-times"></i>
         </div>
+        <div class="container">
+          <div class="text-container">
+            <div class="buttons">
+              <h4>{{player.player.age}} ans</h4>
+              <h4>{{player.player.weight}}</h4>
+              <h4>{{player.player.height}}</h4>
+              <h4>{{player.statistics[0].goals.total}} buts</h4>
+            </div>
+            <div class="location">
+              <i class="fas fa-map-marker-alt"></i>
+              <span>{{player.player.nationality}}</span>
+            </div>
+          </div>
+          <img :src="(player.player.photo)" alt="">
+        </div>
 
       </div>
     </div>
