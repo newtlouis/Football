52b4c0f9a8eb34868ed1a2ce17876b6fcb31fc70
Louis <louis.m.newton@hotmail.fr>
2021-11-18 11:40:09 +0100

    Création d'un serveur nodeJs cacher la clé secrete API

--- assets/js/vue.js
+++ assets/js/vue.js
@@ -7,24 +7,14 @@ const vue = new Vue({
     },
 
     methods: {
-        removeItem(id){
+        removeItem(id) {
             this.$delete(this.players, id);
         }
     },
 
     mounted() {
-        const options = {
-            method: 'GET',
-            url: 'https://api-football-v1.p.rapidapi.com/v3/players',
-            params: {team: '33', season: '2020'},
-            headers: {
-                'x-rapidapi-host': 'api-football-v1.p.rapidapi.com',
-                'x-rapidapi-key': '570ee44315msh453c11a4ef76cc2p1ae9c9jsned1df843a578'
-            }
-        };
-
-        axios.request(options)
-            .then(res => this.players = res.data.response)
+        axios.get("http://localhost:3001/api/search/")
+            .then(res => this.players = res.data.message)
             .catch(e => console.log(e));
     }
 }).$mount("#vue-app");
